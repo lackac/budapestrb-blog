98d946bf38679c87ada54fd63dcc2aaba91496d5
Laszlo Bacsi <github@lackac.name>
2008-09-25 00:15:50 +0200

    some small checks before deployment

--- deploy_app.rb
+++ deploy_app.rb
@@ -4,20 +4,41 @@ require 'json'
  
 class DeployApp
   DEPLOY_DIR = File.expand_path(File.dirname(__FILE__))
+  SEMAFOR = File.join(DEPLOY_DIR, ".deploying")
 
   # Does what it says on the tin. By default, not much, it just prints the
   # received payload.
   def handle_request
-    payload = @req.POST["payload"]
+    return not_found unless request_is_ok?
 
-    return not_found if payload.nil?
+    payload = JSON.parse(@req.POST["payload"])
 
-    payload = JSON.parse(payload)
-
-    %x{cd #{DEPLOY_DIR} && git pull && touch tmp/restart.txt}
-
+    FileUtils.touch(SEMAFOR)
+    deploy
+    FileUtils.rm(SEMAFOR)
     @res.write "Deploying in progress, check back later"
   end
+
+  # Is it a correct request and aren't we deploying already
+  def request_is_ok?
+    if @req.POST["payload"].nil?
+      false
+    elsif @req.path_info != '/deploy'
+      puts "DeployApp: POST with payload but not to '/deploy'"
+      false
+    elsif File.exists?(SEMAFOR)
+      puts "DeployApp: Another deploy already in progress"
+      false
+    else
+      true
+    end
+  end
+
+  # The actual deploy
+  def deploy
+    %x{cd #{DEPLOY_DIR} && git pull > /dev/null && touch tmp/restart.txt}
+  end
+
 
   # Not Found response to non-postcommit requests
   def not_found
