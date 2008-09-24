97d021e48c0a25b71fc3559305fca7c3cc4bc220
Laszlo Bacsi <github@lackac.name>
2008-09-24 23:40:45 +0200

    removing Rack extensions because of a bug simple deploy implementation

--- config.ru
+++ config.ru
@@ -2,6 +2,4 @@
 
 require 'deploy_app'
 
-use Rack::CommonLogger
-use Rack::Lint
 run DeployApp.new
--- deploy_app.rb
+++ deploy_app.rb
@@ -3,20 +3,26 @@ require 'rack'
 require 'json'
  
 class DeployApp
+  DEPLOY_DIR = File.expand_path(File.dirname(__FILE__))
+
   # Does what it says on the tin. By default, not much, it just prints the
   # received payload.
   def handle_request
     payload = @req.POST["payload"]
 
-    return "nil" if payload.nil?
-
-    puts payload unless $TESTING # remove me!
+    return not_found if payload.nil?
 
     payload = JSON.parse(payload)
 
-    # ... Your code goes here! ...
+    %x{cd #{DEPLOY_DIR} && git pull && touch tmp/restart.txt}
 
     @res.write "Deploying in progress, check back later"
+  end
+
+  # Not Found response to non-postcommit requests
+  def not_found
+    @res.status = 404
+    @res.write "nil => 404"
   end
 
   # Call is the entry point for all rack apps.
