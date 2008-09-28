f30b8194459e8af6f2ed97f4b6f1631fa456e743
Laszlo Bacsi <github@lackac.name>
2008-09-28 19:10:29 +0200

    added comment_thread helper and comment counter in layout

---
+++ lib/disqus_helper.rb
@@ -0,0 +1,11 @@
+module DisqusHelpers
+  def comment_thread
+    %{
+      <div id="disqus_thread"></div>
+      <script type="text/javascript" src="http://disqus.com/forums/budapest-rb/embed.js"></script>
+      <noscript><a href="http://budapest-rb.disqus.com/?url=ref">Kommentek megtekintése.</a></noscript>
+    }
+  end
+end
+
+Webby::Helpers.register(DisqusHelpers)
