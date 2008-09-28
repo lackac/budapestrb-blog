125bff5d36344f9b99325a8b4c289d3578b0597a
Laszlo Bacsi <github@lackac.name>
2008-09-28 19:13:10 +0200

    moved comment counter code to a helper

--- lib/disqus_helper.rb
+++ lib/disqus_helper.rb
@@ -6,6 +6,26 @@ module DisqusHelpers
       <noscript><a href="http://budapest-rb.disqus.com/?url=ref">Kommentek megtekintése.</a></noscript>
     }
   end
+
+  def comment_counter
+    %{
+<!-- counting comments with disqus -->
+<script type="text/javascript">
+//<![CDATA[
+(function() {
+  var links = document.getElementsByTagName('a');
+  var query = '?';
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf('#disqus_thread') >= 0) {
+      query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+    }
+  }
+  document.write('<script type="text/javascript" src="http://disqus.com/forums/budapest-rb/get_num_replies.js' +        query + '"></' + 'script>');
+})();
+//]]>
+</script>
+    }
+  end
 end
 
 Webby::Helpers.register(DisqusHelpers)
